03ef92f271d67e70dc447a859ba07bdb1ad9c872
Adam Driggers <awdriggs@gmail.com>
2023-02-09 00:10:26 -0500

    css needs help

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 # save this as app.py
 from flask import Flask
 from flask import render_template
-from helpers import *
-
-print(stupid())  
+from helpers import build #here be the cellular automata function
 
 app = Flask(__name__)
 
@@ -17,24 +15,25 @@ def about():
 
 @app.route('/ca/<int:number>')
 def ca(number):
-    return render_template("test.html", num = number)
+    board = build(number, False) #builds the ca space as a list of rows, each row is a string 
+    return render_template("test.html", num = number, board = board)
 
-@app.context_processor
-def inject_rules():
-    rules = ["111", "110", "101", "100", "011", "010", "001", "000"] #the rules
-    return dict(user="adam", rules=rules)
+# @app.context_processor
+# def inject_rules():
+#     rules = ["111", "110", "101", "100", "011", "010", "001", "000"] #the rules
+#     return dict(user="adam", rules=rules)
 
-@app.context_processor
-def utility_processor():
-    def format_price(amount, currency="€"):
-        return f"{amount:.2f}{currency}"
-    def get_set(num):
-        return list("{:08b}".format(num)) #list of the binary representation of the rule set number as a sring
-    return dict(format_price=format_price, get_set=get_set)
+# @app.context_processor
+# def utility_processor():
+#     def format_price(amount, currency="€"):
+#         return f"{amount:.2f}{currency}"
+#     def get_set(num):
+#         return list("{:08b}".format(num)) #list of the binary representation of the rule set number as a sring
+#     return dict(format_price=format_price, get_set=get_set)
 
-# functions for the templates
-def clever_function():
-    return u'HELLO'
+# # functions for the templates
+# def clever_function():
+#     return u'HELLO'
 
-app.jinja_env.globals.update(clever_function=clever_function, stupid=stupid)
+# app.jinja_env.globals.update(clever_function=clever_function, stupid=stupid, get_seed=get_seed, automate=automate)
  
